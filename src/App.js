bb3eb10465d0ddea9380c27f12958a0f2fd127a3
VaibhavDabral11 <vaibhavdabral44@gmail.com>
2023-02-22 12:13:46 +0530

    intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Login from "./Components/Login";
+import Rgister from "./Components/Rgister";
+import Home from "./Components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Login" element={< Login />} />
+        <Route path="/home" element={< Home />} />
+        <Route path="/Register" element={<Rgister />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Rgister.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rgister = () => {
+  return (
+    <div>
+      <h2>This is the register page </h2>
+    </div>
+  )
+}
+
+export default Rgister
